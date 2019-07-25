2d23ec6c48d2cee361acbc76dce5e902169c6dc1
Tomek Bar <tomasz.bar@deviniti.com>
2019-07-25 14:56:05 +0200

    first commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+7
+pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
+c
+3src/main/java/com/deviniti/jirainstaller2/Main.java,2/8/280bb22f48a0655491aa33739289a3377874664a
---
+++ src/main/java/com/deviniti/jirainstaller2/Main.java
@@ -0,0 +1,26 @@
+package com.deviniti.jirainstaller2;
+
+import expectj.ExpectJ;
+import expectj.Spawn;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        String installationBin = args[0];
+        String jiraInstallDirectory = args[1];
+        String jiraHome = args[2];
+
+        ExpectJ expectinator = new ExpectJ(10);
+        Spawn shell = expectinator.spawn(installationBin);
+        shell.expect("OK [o, Enter], Cancel [c]");
+        shell.send("o \n");
+        shell.expect("Express Install");
+        shell.send("1 \n");
+        shell.expect("Where should JIRA Software be installed?");
+        shell.send(jiraInstallDirectory + "\n");
+        shell.expect("Default location for JIRA Software data");
+        shell.send(jiraHome + "\n");
+        shell.expect("Install as service: No");
+        shell.send("i \n");
+    }
+}
